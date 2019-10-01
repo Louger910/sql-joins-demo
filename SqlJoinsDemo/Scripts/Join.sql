b3a745a593aaa43ac40361046883938be4158df3
livalex <livalex11@gmail.com>
2019-10-01 20:41:42 +0300

    hm

---
+++ SqlJoinsDemo/Scripts/Join.sql
@@ -0,0 +1,4 @@
+﻿SELECT [c].[name] AS 'Город', [s].[name] AS 'Название поставщика 1', [s2].[name] AS 'Назвние поставщика 2'
+From [Cities] AS [c]
+	JOIN [Suppliers] AS [s] ON ([c].[id]= [s].[city_id])
+	JOIN [Suppliers] AS [s2] ON ([s].[city_id]= [s2].[city_id])
